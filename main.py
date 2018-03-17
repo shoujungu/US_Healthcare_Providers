f1c5ce80a5ad976993d73f1f1d380c1b0c46f42f
Shoujun <shoujun.gu@gmail.com>
2018-03-16 23:06:21 -0400

    5th

--- main.py
+++ main.py
@@ -83,5 +83,5 @@ def get_pie_data(state):
 #run
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT'))
     app.run(host='0.0.0.0', port=port)
